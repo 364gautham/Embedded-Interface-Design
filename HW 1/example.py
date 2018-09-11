0a86013e5bb56dabc6b5684879997646f7f98d4e
364gautham <gaka6687@colorado.edu>
2018-09-11 12:12:36 -0600

    Required changes for HW1 completion

--- HW 1/example.py
+++ HW 1/example.py
@@ -8,7 +8,7 @@ Non-public methods should at least have a comment that describes what
 the method does.  (See https://www.python.org/dev/peps/pep-0008/ for more information.)
 
 When a doc string is used, this will be information will be available as the __doc__
-attribute for this module.  
+attribute for this module.
 
 On the python command line, you could type
 >>> import example
@@ -19,38 +19,42 @@ import sys
 import os
 import datetime
 
-__author__ = "Bruce Montgomery"
-__copyright__ = "Copyright (C) 2018 by Bruce Montgomery"
+__author__ = "Gautham K A"
+__copyright__ = "Copyright (C) 2018 by Gautham K A"
+
+
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Bruce Montgomery and the University of Colorado
+# software. Gautham K A and the University of Colorado
 # are not liable for any misuse of this material.
 #
 
 class Dog(object):
     """Dog: an example of a Python class from https://docs.python.org/3/tutorial/classes.html"""
-    kind = 'canine'			# class variable for all dogs
+    kind = 'canine'  # class variable for all dogs
 
     def __init__(self, name):
         """Returns a Dog object with a name and an empty list of tricks"""
         self.name = name
-        self.tricks = []    # creates a new empty list for each dog
+        self.tricks = []  # creates a new empty list for each dog
 
     def add_trick(self, trick):
         """Adds a new trick to a Dog's list of tricks"""
         self.tricks.append(trick)
 
+
 def walker(name):
     """walker: an example of a python function"""
     dog1 = Dog(name)
-    dog1.add_trick("Roll Over")
+    dog1.add_trick("Play Dead")
     print("Walking a new dog:")
-    print("  The kind of this thing is ",dog1.kind)
-    print("  This dog is ",dog1.name)
-    print("  It knows these tricks - ",dog1.tricks)
-			
+    print("  The kind of this thing is ", dog1.kind)
+    print("  This dog is ", dog1.name)
+    print("  It knows these tricks - ", dog1.tricks)
+
+
 # You can run this code from a command line - something like
 # C:\Source> python example.py Fido
 #    ...runs walker on the name Fido...
@@ -62,4 +66,4 @@ def walker(name):
 #    ...runs walker on the name Fred...
 
 if __name__ == "__main__":
-    walker((sys.argv[1]))
+    walker(sys.argv[1])
